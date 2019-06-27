cdfa3a3795f8d2f9126850e3763add248d2de1fe
tom <tom@163.com>
2019-06-27 20:47:34 +0800

    基础路由跳转版本

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>这是登录页面，由于功能尚未完善，<br>
+      所以此处忽略登录功能，直接通过点击进入后台管理首页</h3>
+    <router-link to="/admin">进入后台管理首页</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login'
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import AdminIndex from '@/components/AdminIndex'
+import ProductManage from '@/components/ProductManage'
+import ClientManage from '@/components/ClientManage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/admin',
+      name: 'AdminIndex',
+      component: AdminIndex
+    },
+    {
+      path: '/product',
+      name: 'ProductManage',
+      component: ProductManage
+    },
+    {
+      path: '/client',
+      name: 'ClientManage',
+      component: ClientManage
+    }
+  ]
+})
